ced354878a577f518c336d95889b4be9df0cbdfa
zerocoolrules <mrericemmanuelbailey@gmail.com>
2023-06-29 19:24:33 -0500

    Week 3 Activity

--- Week3/3.1/practiceFunction.js
+++ Week3/3.1/practiceFunction.js
@@ -6,3 +6,17 @@ nameFunction();
 
 myFirstFunction;
 myFirstFunction();
+
+
+// SCOPE //////////////////////////////////////////////////////////////////////////////
+// GLOBAL
+const x = 5;
+
+function myFunction(...params) {
+  // variable inside the scope of myFunction
+  let scopedVariable = 10;
+  if (scopedVariable) {
+    // a child scope of myFunction's scope
+    let childVariable = scopedVariable + 5;
+  }
+}
